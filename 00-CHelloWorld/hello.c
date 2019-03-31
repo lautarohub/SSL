15091558c0cb9cba6db9aa898fca3f3835981c16
Lautaro <lautaro.aguilera43@gmail.com>
2019-03-31 13:31:17 -0300

    hello.c

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,10 @@
+/* Trabajo practico 0, SSL
+ * Programa que imprime por consola "Hello, world!"
+ * Autor: Lautaro Aguilera
+ * Fecha: 31/03/2019
+*/
+#include <stdio.h>
+
+int main(){
+   printf("Hello, World!\n");
+}
